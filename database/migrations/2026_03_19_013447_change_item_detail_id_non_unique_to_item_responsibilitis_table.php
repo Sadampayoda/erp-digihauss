dd0f9136c683fd13e57f2df2c7726374b0b7053a
si prik tapi suka anin <111823958+Sadampayoda@users.noreply.github.com>
2026-03-19 17:34:28 +0700

    bug: unique item_detail_id

--- database/migrations/2026_03_19_013447_change_item_detail_id_non_unique_to_item_responsibilitis_table.php
+++ database/migrations/2026_03_19_013447_change_item_detail_id_non_unique_to_item_responsibilitis_table.php
@@ -12,7 +12,19 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('item_responsibilitis', function (Blueprint $table) {
+            $table->dropForeign(['item_detail_id']);
+
+            // 2. Drop unique index
             $table->dropUnique(['item_detail_id']);
+
+            // 3. (optional) jadi index biasa
+            $table->index('item_detail_id');
+
+            // 4. Tambahin lagi foreign key
+            $table->foreign('item_detail_id')
+                ->references('id')
+                ->on('item_details')
+                ->cascadeOnDelete();
         });
     }
 
@@ -22,7 +34,15 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('item_responsibilitis', function (Blueprint $table) {
-            //
+            $table->dropForeign(['item_detail_id']);
+            $table->dropIndex(['item_detail_id']);
+
+            $table->unique('item_detail_id');
+
+            $table->foreign('item_detail_id')
+                ->references('id')
+                ->on('item_details')
+                ->cascadeOnDelete();
         });
     }
 };
