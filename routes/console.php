ccd4a23692f91e63ebc91d4fc21e4297881550b4
si prik tapi suka anin <111823958+Sadampayoda@users.noreply.github.com>
2026-03-21 19:33:53 +0700

    fix: add log closing

--- routes/console.php
+++ routes/console.php
@@ -11,12 +11,9 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('app:auto-create-for-next-day-item-responbility')
-    ->dailyAt(setting('closing_day_time'))
-    ->withoutOverlapping()
-    ->appendOutputTo(storage_path('logs/cron.log'));
 
 Schedule::call(function () {
+    Log::info('closing day: '.now());
     $lock = Cache::lock('auto-create-item-responsibility', 3600);
 
     if (!$lock->get()) {
